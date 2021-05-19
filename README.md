362de52e5fa1911194c3eb64df43de76be356e22
winner202105 <84320170+winner202105@users.noreply.github.com>
2021-05-20 06:07:02 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# CCI-GCR-Currency0517
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+// Currency conversion table
+const currencyArray = [
+    { unit: "JPY", rate: 1 },
+    { unit: "USD", rate: 104.49 },
+    { unit: "EUR", rate: 124.86 },
+    { unit: "CNY", rate: 15.21 },
+    { unit: "KRW", rate: 0.0892 },
+    { unit: "AUD", rate: 83.62 }, 
+  ];
+  
+// API Implementations
+const app = express();
+app.use(express.json());
+
+app.get('/api/list', (req, res) => {
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(200).send(currencyArray);
+});
+
+app.get('/api/convert', (req, res) => {
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    var currency = currencyArray.find(c => c.unit === req.query.unit);
+    if (! currency) return res.status(200).send({'error': 'unitNotSupported'});
+
+    var value = parseFloat(req.query.value);
+    var amount = value * currency.rate;
+    res.status(200).send({ 'amount': amount });
+});
+
+// Server
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0'
+module.exports = app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
